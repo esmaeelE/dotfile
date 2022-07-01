7439a234b51bfe679eb2209abaf0148f3d71028c
esmaeelE <esmaeelEE@protonmail.com>
2022-07-02 00:36:37 +0430

    Create anothervim.sh

---
+++ anothervim.sh
@@ -0,0 +1,71 @@
+"support bidirectional language
+set termbidi
+
+"enable mouse
+set mouse=a
+
+" enable syntax highlighting
+syntax enable
+" syntax on what is differ
+
+" show line numbers
+" set number
+
+" set tabs to have 4 spaces
+set ts=4
+
+" indent when moving to the next line while writing code
+set autoindent
+
+" expand tabs into spaces
+set expandtab
+
+" when using the >> or << commands, shift lines by 4 spaces
+set shiftwidth=4
+
+" show the matching part of the pair for [] {} and ()
+set showmatch
+
+set encoding=UTF-8
+
+" change cursor insert and normal mode
+let &t_SI = "\<esc>[5 q"
+let &t_SR = "\<esc>[3 q"
+let &t_EI = "\<esc>[2 q"
+
+let g:NERDTreeWinPos = "right"
+
+" esc in insert mode
+inoremap kj <esc>
+
+set hlsearch " Highlight in search
+
+syntax on
+set t_Co=256
+set cursorline
+
+" relativ number in normal and abs in insert mode
+set number
+augroup numbertoggle
+  autocmd!
+  autocmd BufEnter,FocusGained,InsertLeave,WinEnter * if &nu && mode() != "i" | set rnu   | endif
+  autocmd BufLeave,FocusLost,InsertEnter,WinLeave   * if &nu                  | set nornu | endif
+augroup END
+
+filetype plugin indent on
+set autoindent
+set cindent
+
+" kernel newbie expressions
+"
+
+filetype plugin indent on
+syntax on
+set title
+set tabstop=8
+set softtabstop=8
+set shiftwidth=8
+set noexpandtab
+
+""
+colorscheme desert
